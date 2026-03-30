82d434f9c93e6b75718f1efc082b83e7b841d735
Biel <alexandregabrielsouza30@gmail.com>
2026-03-30 10:17:56 -0300

    feat: exercicios de operadores

--- src/js/script.js
+++ src/js/script.js
@@ -51,3 +51,46 @@ console.log(exemplo7)
 //valor vazio 
 let exemplo8 = null;
 console.log(exemplo8)
+
+//  OPERADORES ARITMÉTICOS 
+const valor1 = 10;
+const valor2 = 20;
+
+console.log (valor1 + valor2);
+console.log (valor1 - valor2);
+console.log (valor1 * valor2);
+console.log (valor1 / valor2);
+
+// OPERADORES LÓGiCOS
+
+console.log ( valor1 < valor2);
+console.log (valor1  > valor2);
+console.log (valor1 > 10 || valor2 < 10)
+
+//******************************************EXERCICIO 1 */
+
+const p=20;
+const v=10;
+
+const precoFinal = p -v;
+console.log ("O preço final com desconto é ", precoFinal )
+
+//********************************************************
+// EXERCICIO 2 
+//  */
+
+    let a= 20;
+    let b = 10;
+
+    let comparar = (a ==2 && b >=10 );
+    console.log ("O resultado é:", comparar);
+
+
+    // ***************************EXERCICIO 3 
+
+    const usuariologado=20;
+    const tempoToken= 10;
+
+    const transacao = (usuariologado===20 && tempoToken >=10)
+    console.log("Transação Autorizada", transacao)
+    
